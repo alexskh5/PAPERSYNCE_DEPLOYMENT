59987fc5a59b270766328f2fdd26f2518f3f8bc5
Sam Alexies H. Dela Peña <samalexies.delapena@ctu.edu.ph>
2025-06-15 22:23:21 +0800

    Update ProposeMeasure.py

--- views/ProposeMeasure.py
+++ views/ProposeMeasure.py
@@ -40,7 +40,8 @@ class ProposeMeasureApp:
         
         # Local development path (default)
         #self.UPLOADS_DIR = os.path.join(PROJECT_ROOT, "uploads")
-        self.UPLOADS_DIR = r"C:\paperSync\uploads"
+        # self.UPLOADS_DIR = r"C:\paperSync\uploads"
+        self.UPLOADS_DIR = r"\\MCKHENZYY\uploads2"
         # Uncomment this when deploying to client and NAS is mounted
         # self.UPLOADS_DIR = "/mnt/nas/uploads"  # For Linux
         # self.UPLOADS_DIR = "/Volumes/NASShare/uploads"  # For macOS
