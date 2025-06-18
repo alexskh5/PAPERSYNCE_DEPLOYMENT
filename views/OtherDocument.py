f77fac1d066196cfeb0320064f8030c6f71057e0
Sam Alexies H. Dela Peña <samalexies.delapena@ctu.edu.ph>
2025-06-18 22:18:45 +0800

    Update OtherDocument.py

--- views/OtherDocument.py
+++ views/OtherDocument.py
@@ -42,7 +42,7 @@ class OtherDocumentApp:
         # Local development path (default)
         # self.UPLOADS_DIR = os.path.join(PROJECT_ROOT, "uploads")
         # self.UPLOADS_DIR = r"C:\paperSync\uploads"
-        self.UPLOADS_DIR = r"\\192.168.1.12\uploads2"
+        self.UPLOADS_DIR = r"\\192.168.1.100\uploads2"
         # Uncomment this when deploying to client and NAS is mounted
         # self.UPLOADS_DIR = "/mnt/nas/uploads"  # For Linux
         # self.UPLOADS_DIR = "/Volumes/NASShare/uploads"  # For macOS
